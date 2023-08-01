5bda6b7bcd0123b7420599bdac98e67001984c1b
Yusuf Kula <yvk2045@gmail.com>
2023-08-01 22:25:37 +0100

    Player movement and score system updated

--- character.py
+++ character.py
@@ -1,10 +1,12 @@
 import pygame
 
-class Character():
-    def __init__(self, display):
+
+class RunnerCharacter:
+    def __init__(self):
         # super().__init__()
         self.player_image = pygame.image.load("player_stand.png").convert_alpha()
-        self.player_image_rect = self.player_image.get_rect(center=(400, 200))
+        self.player_image_rect = self.player_image.get_rect(center=(50, 260))
 
     def move_character(self):
         self.player_image_rect.x += 1
+
--- main.py
+++ main.py
@@ -18,6 +18,7 @@ ground = pygame.image.load("ground.png").convert()
 ground_rect = ground.get_rect(midtop=(400, 300))
 
 # player character
+player_gravity = 0
 player = RunnerCharacter()
 
 score = Score()
@@ -25,6 +26,10 @@ score = Score()
 game_font = pygame.font.Font("Pixeltype.ttf", 50)
 score_text = game_font.render(f"Score:{score}", False, (64, 64, 64))
 score_text_rect = score_text.get_rect(center=(400, 100))
+
+vel = 5
+
+keys = pygame.key.get_pressed()
 
 # clock object
 clock = pygame.time.Clock()
@@ -37,11 +42,28 @@ while True:
             score.save_score()
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.player_image_rect.y -= vel
+
+
 
     screen.blit(ground, ground_rect)
     score.update_score()
     screen.blit(score.score_text,score.score_text_rect)
+
+
+    # Player
+    player_gravity += 1
+    player.player_image_rect.y += player_gravity
+    if player.player_image_rect.y >= 215:
+        player.player_image_rect.y = 215
+
     screen.blit(player.player_image, player.player_image_rect)
-    player.move_character()
+    # player.move_character()
     pygame.display.update()
     clock.tick(60)
+
+
+
+
